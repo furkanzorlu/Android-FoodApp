ddc459db65e8b35825f653f03ebe0a03ebc579ba
furkanzorlu <furkan__zorlu@hotmail.com>
2022-02-21 16:29:28 +0300

    a

--- foodapp2/app/src/main/java/com/example/foodapp/entity/Comment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.foodapp.entity
-
-import com.google.firebase.database.IgnoreExtraProperties
-import java.io.Serializable
-
-@IgnoreExtraProperties
-class Comment (var comment_id:String? = "",var comment_name:String? = "",var comment_user_name:String? = "") : Serializable {
-}
