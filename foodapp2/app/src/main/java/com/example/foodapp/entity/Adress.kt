16e36c9b688d9315de59b276c2ee3108c1b6ee3f
furkanzorlu <49489211+furkanzorlu@users.noreply.github.com>
2022-02-21 16:31:19 +0300

    Delete foodapp2 directory

--- foodapp2/app/src/main/java/com/example/foodapp/entity/Adress.kt
+++
@@ -1,8 +0,0 @@
-package com.example.foodapp.entity
-
-import com.google.firebase.database.IgnoreExtraProperties
-import java.io.Serializable
-
-@IgnoreExtraProperties
-data class Adress (var adress_id:String? = "",var adress_name:String? = "") : Serializable {
-}
